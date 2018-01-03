df90bc1f1a0388296216120202b266c403b7a73c
afeiship <1290657123@qq.com>
2018-01-03 09:36:38 +0800

    issue: initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# next-detect-android
+> Detect if android &amp; version.
+
+
+## resources:
+
+## todos:
+- [ ] unit test case
---
+++ src/next-detect-android.js
@@ -0,0 +1,30 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+  var USER_AGENT = global.navigator.userAgent;
+  var ANDROID_RE = /Android/;
+  var Android_STR = 'Android ';
+  var SEMICOLON = ';';
+
+  var NxDetectAndroid = nx.declare('nx.DetectAndroid', {
+    statics:{
+      isAndroid : function () {
+        return ANDROID_RE.test(USER_AGENT);
+      },
+      version: function(){
+        var splitUserAgent = USER_AGENT.split(Android_STR);
+        if (splitUserAgent.length > 1) {
+          return splitUserAgent[1].split(SEMICOLON)[0];
+        }
+        return null;
+      }
+    }
+  });
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxDetectAndroid;
+  }
+
+}());
